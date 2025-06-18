7445a2795be21be16599b853373dcd349a8f69d8
Swarnim <135417168+swarnimk-10@users.noreply.github.com>
2025-06-18 17:18:11 +0530

    Update pc_reg_adder.v

--- pc_reg_adder.v
+++ pc_reg_adder.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.06.2025 11:12:31
-// Design Name: 
-// Module Name: pc_reg_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module pc_reg_adder (
