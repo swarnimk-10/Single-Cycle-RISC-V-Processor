8afb607f1a4d46cb0a276b6aeb477a63002bc042
Swarnim <135417168+swarnimk-10@users.noreply.github.com>
2025-06-18 22:25:17 +0530

    Update register_file.v

--- register_file.v
+++ register_file.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.06.2025 11:37:44
-// Design Name: 
-// Module Name: register_file
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module register_file (
     input clk,
