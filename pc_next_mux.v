47f9c86f0a5cb4e230b8086a17f0c427e4917a2e
Swarnim <135417168+swarnimk-10@users.noreply.github.com>
2025-06-18 17:13:47 +0530

    Update pc_next_mux.v

--- pc_next_mux.v
+++ pc_next_mux.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.06.2025 11:22:58
-// Design Name: 
-// Module Name: pc_next_mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module pc_next_mux (
     input [31:0] PCPlus4,    // PC + 4 (normal next)
