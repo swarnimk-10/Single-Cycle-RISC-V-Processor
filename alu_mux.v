b46f4814a26a51f86379ee7f3d4d6478239fc9b3
Swarnim <135417168+swarnimk-10@users.noreply.github.com>
2025-06-19 11:13:07 +0530

    Update alu_mux.v

--- alu_mux.v
+++ alu_mux.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.06.2025 14:37:53
-// Design Name: 
-// Module Name: alu_mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module alu_mux (
     input [31:0] RD2,
