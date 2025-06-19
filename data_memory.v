859bd7eb839f3b44a70e4f54265c1c82ff08b3e4
Swarnim <135417168+swarnimk-10@users.noreply.github.com>
2025-06-19 11:24:11 +0530

    Update data_memory.v

--- data_memory.v
+++ data_memory.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.06.2025 15:02:26
-// Design Name: 
-// Module Name: data_memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module data_memory (
     input        clk,
